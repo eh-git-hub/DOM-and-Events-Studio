86cea4d959646a26140a70852f6e61538cdd4b16
Edgar Hernandez <EdgarHomeFolder@Edgars-MacBook-Pro.local>
2020-11-30 20:37:44 -0600

    added correct function for rocket img to move

--- scripts.js
+++ scripts.js
@@ -8,11 +8,14 @@ upButton = document.getElementById("up");
 downButton = document.getElementById("down");
 leftButton = document.getElementById("left");
 rightButton = document.getElementById("right");
+rocket = document.getElementById("rocket");
+rocket.style.bottom = '0';
+rocket.style.left = '0';
 
 function onClickTakeOff(){
     if (window.confirm("Is the shuttle ready for takeoff?") === true){
         document.getElementById("flightStatus").innerHTML = "Shuttle in flight!"
-        document.getElementById("shuttleBackground").style.background = "#3457D5"; //blue
+        document.getElementById("shuttleBackground").style.background = "#318CE7"; //blue de france
         document.getElementById("spaceShuttleHeight").innerHTML = "10,000";
     }
 }
@@ -20,35 +23,38 @@ function onClickTakeOff(){
 function onClickLand(){
     window.alert("The shuttle is landing. Landing gear engaged!");
     document.getElementById("flightStatus").innerHTML = "Shuttle has landed!";
-    document.getElementById("shuttleBackground").style.background = "#568203"; //green
+    document.getElementById("shuttleBackground").style.background = "#568203"; //avocado green
     document.getElementById("spaceShuttleHeight").innerHTML = "0";
 }
 
 function onClickMissionAbort(){
     if (window.confirm("Do you want to abort the mission?") === true){
         document.getElementById("flightStatus").innerHTML = "Mission aborted!"
-        document.getElementById("shuttleBackground").style.background = "#568203"; //green
+        document.getElementById("shuttleBackground").style.background = "#FF0800"; //candy apple red
         document.getElementById("spaceShuttleHeight").innerHTML = "0";
     }
 }
 
 
 function onClickUpButton(){
-    document.getElementById("rocket").style.top = "100px";
-}
+    movement = parseInt(rocket.style.bottom) + 10 + "px";
+    rocket.style.bottom = movement;
+};
 
 function onClickDownButton(){
-    document.getElementById("rocket").style.top = "100px";
-}
+    movement = parseInt(rocket.style.bottom) - 10 + "px";
+    rocket.style.bottom = movement;
+};
 
 function onClickLeftButton(){
-    document.getElementById("rocket").style.left = "100px";
-}
+    movement = parseInt(rocket.style.left) - 10 + "px";
+    rocket.style.left = movement;
+};
 
 function onClickRightButton(){
-    document.getElementById("rocket").style.right = "100px";
-
-}
+    movement = parseInt(rocket.style.left) + 10 + "px";
+    rocket.style.left = movement;
+};
 
 
 takeoffButton.addEventListener("click", onClickTakeOff);
